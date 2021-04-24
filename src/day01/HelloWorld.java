4962cc605c9315c8c4ef5c1db562711e1e6b20aa
Muhammad Qureshi <muhammadqureshi1996@gmail.com>
2021-04-24 16:52:42 -0400

    Added more work after publishing to github

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello World");
 
         System.out.println("Adding Github now");
+        System.out.println("YAY STUFFS ARE ADDED ON GITHUB");
     }
 }
